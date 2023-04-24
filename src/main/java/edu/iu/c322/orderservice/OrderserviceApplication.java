623072207a1a94d61d10a7d747ab24f4ce2fabb6
evtomak <evtomak@iu.edu>
2023-04-24 07:58:26 -0400

    Push.

--- src/main/java/edu/iu/c322/orderservice/OrderserviceApplication.java
+++ src/main/java/edu/iu/c322/orderservice/OrderserviceApplication.java
@@ -2,6 +2,7 @@ package edu.iu.c322.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class OrderserviceApplication {
@@ -9,5 +10,4 @@ public class OrderserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderserviceApplication.class, args);
     }
-
 }
